9682bc4af04c9801aae376ee0c7be53e759e8e8a
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-09-15 10:47:26 -0300

    🆕 - Created new functions to interact with command modals
    It were created functions to open and close modals; enable and disable scrolling.

--- views/scripts/popup.js
+++ views/scripts/popup.js
@@ -1,5 +1,62 @@
-function showDetailsPopup(command, description) {
-  modal = document.getElementById(command);
+let modal;
+
+function showDetailsPopup(command) {
+  modal = document.getElementById("modal" + command);
 
   modal.style.display = "block";
+  disableScroll();
 }
+
+//close = document.getElementById("close" + commName);
+
+function closeModal(commName) {
+  modalC = document.getElementById("modal" + commName);
+  modalC.style.display = "none";
+  enableScroll();
+}
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//close.onclick = function() {
+    //modal.style.display = "none";
+//}
